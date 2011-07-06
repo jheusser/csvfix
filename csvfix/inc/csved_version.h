cd800028e013065405083f19051cb1db5b89dc37
neilb <neilb@localhost>
2011-07-06 18:42:35 +0100

    updated manual to cover diff & file_merge bumped version

--- csvfix/inc/csved_version.h
+++ csvfix/inc/csved_version.h
@@ -10,8 +10,8 @@
 #define INC_CSVED_VERSION_H
 
 const char * const CSVED_NAME = "CSVfix";
-const char * const CSVED_VERS = "1.00";
-const char * const CSVED_CPYR = "Copyright (C) 2010 Neil Butterworth";
+const char * const CSVED_VERS = "1.1";
+const char * const CSVED_CPYR = "Copyright (C) 2011 Neil Butterworth";
 
 #endif
 
