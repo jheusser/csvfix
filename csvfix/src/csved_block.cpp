4f53f333960afa8fa83581258084181beea58a09
neilb <neilb@localhost>
2012-03-23 10:27:14 +0000

    initial block options working

--- csvfix/src/csved_block.cpp
+++ csvfix/src/csved_block.cpp
@@ -10,6 +10,7 @@
 #include "a_collect.h"
 #include "csved_cli.h"
 #include "csved_block.h"
+#include "csved_evalvars.h"
 #include "csved_strings.h"
 #include <string>
 using std::string;
@@ -43,9 +44,10 @@ const char * const BLOCK_HELP = {
 //----------------------------------------------------------------------------
 
 BlockCommand :: BlockCommand( const string & name, const string & desc )
-				: Command( name, desc, BLOCK_HELP ) {
+				: Command( name, desc, BLOCK_HELP ) ,
+					mAction( None ), mExclusive( false ) {
 
-	AddFlag( ALib::CommandLineFlag( FLAG_BEXPR, false, 1 ) );
+	AddFlag( ALib::CommandLineFlag( FLAG_BEXPR, true, 1 ) );
 	AddFlag( ALib::CommandLineFlag( FLAG_EEXPR, false, 1 ) );
 	AddFlag( ALib::CommandLineFlag( FLAG_ACTKEEP, false, 0 ) );
 	AddFlag( ALib::CommandLineFlag( FLAG_ACTREM, false, 0 ) );
@@ -56,28 +58,75 @@ BlockCommand :: BlockCommand( const string & name, const string & desc )
 //----------------------------------------------------------------------------
 ///----------------------------------------------------------------------------
 
+enum State { Outside, Inside };
+
 int BlockCommand :: Execute( ALib::CommandLine & cmd ) {
+
 	ProcessFlags( cmd );
+	IOManager io( cmd );
+	CSVRow row;
+	State state = Outside;
+	bool block = true;
+
+	while( io.ReadCSV( row ) ) {
+		if ( state == Outside ) {
+			AddVars( mBeginEx, io, row );
+			if ( AtBeginBlock() ) {
+				block = ! mExclusive;
+				state = Inside;
+			}
+			else {
+				block = false;
+			}
+		}
+		else if ( state == Inside ) {
+			AddVars( mEndEx, io, row );
+			if ( AtEndBlock() ) {
+				block = ! mExclusive;
+				state = Outside;
+			}
+			else {
+				block = true;
+			}
+		}
+		else {
+			CSVTHROW( "Bad state" );
+		}
+
+		//std::cout << "Block is " << block << std::endl;
+		if ( (block && mAction == Keep) || (!block && mAction == Remove) ) {
+			io.WriteRow( row );
+		}
+	}
 	return 0;
+}
+
+bool BlockCommand :: AtEndBlock()  {
+	return ALib::Expression::ToBool( mEndEx.Evaluate() );
+}
+
+bool BlockCommand :: AtBeginBlock()  {
+	return ALib::Expression::ToBool( mBeginEx.Evaluate() );
 }
 
 //----------------------------------------------------------------------------
 // Get command line options.
 //----------------------------------------------------------------------------
 
-
 void BlockCommand :: ProcessFlags( const ALib::CommandLine & cmd ) {
-	string beginex = cmd.GetValue( FLAG_BEXPR, "" );
-	if ( beginex != "" ) {
-		mBeginEx.Compile( beginex );
+
+	string beginex = cmd.GetValue( FLAG_BEXPR );
+	string emsg = mBeginEx.Compile( beginex );
+	if ( emsg != "" ) {
+		CSVTHROW( "Invalid expression for " << FLAG_BEXPR );
 	}
-	string endex = cmd.GetValue( FLAG_EEXPR, "" );
-	if ( endex != "" ) {
-		mEndEx.Compile( endex );
+
+	string endex = cmd.GetValue( FLAG_EEXPR, beginex );
+	emsg = mEndEx.Compile( endex );
+	if ( emsg != "" ) {
+		CSVTHROW( "Invalid expression for " << FLAG_EEXPR );
 	}
-	if ( endex == "" && beginex == "" ) {
-		CSVTHROW( "Need " << FLAG_BEXPR << " and/or " << FLAG_EEXPR );
-	}
+
 	if ( cmd.HasFlag( FLAG_ACTREM ) && cmd.HasFlag( FLAG_ACTKEEP ) ) {
 		CSVTHROW( "Can specify only one of "
 						<< FLAG_ACTKEEP << " or " << FLAG_ACTREM );
@@ -85,7 +134,7 @@ void BlockCommand :: ProcessFlags( const ALib::CommandLine & cmd ) {
 	if ( cmd.HasFlag( FLAG_ACTREM ) ) {
 		mAction = Remove;
 	}
-	if ( cmd.HasFlag( FLAG_ACTKEEP ) ) {
+	else if ( cmd.HasFlag( FLAG_ACTKEEP ) ) {
 		mAction = Keep;
 	}
 	else {
@@ -93,6 +142,7 @@ void BlockCommand :: ProcessFlags( const ALib::CommandLine & cmd ) {
 	}
 	mExclusive = cmd.HasFlag( FLAG_BLKEXC );
 }
+
 
 //----------------------------------------------------------------------------
 
