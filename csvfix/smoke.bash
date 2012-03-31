0ceef936435e766cf507b2819583936327c720fc
neilb <neilb@localhost>
2012-03-31 15:39:53 +0100

    minor file clean out

--- csvfix/smoke.bash
+++ csvfix/smoke.bash
@@ -3,16 +3,16 @@ echo "Running..."
 CSVED=../bin/csvfix.exe
 export CSVED
 DIFF=/bin/diff
-DIFFOUT=test/tmp/_diffout
-cd test
+DIFFOUT=tests/tmp/_diffout
+cd tests
 if bash tests/smoke.test > tmp/smoke.test
 then
 	if $DIFF correct/smoke.test tmp/smoke.test
 	then
-		swine msgbox -caption 'CSVFix Smoke Test' -text 'PASSED  ' -icon info	
+		swine msg --caption 'CSVFix Smoke Test' --text 'PASSED  ' --icon info	
 		exit 0
 	else
-		swine msgbox -caption 'CSVFix Smoke Test' -text 'FAILED' -icon stop	
+		swine msg --caption 'CSVFix Smoke Test' --text 'FAILED' --icon stop	
 		exit 1
 	fi
 else
