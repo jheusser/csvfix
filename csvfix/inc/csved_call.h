bb664398716055eeb6efe050fa439b110453552e
Neil Butterworth <nbutterworth1953@gmail.com>
2012-07-22 12:27:31 +0100

    inital function load and call working for 'call' command

--- csvfix/inc/csved_call.h
+++ csvfix/inc/csved_call.h
@@ -15,8 +15,20 @@
 
 namespace CSVED {
 
-class CallCommand : public Command {
 
+//----------------------------------------------------------------------------
+// Function in DLL needs to look like this
+//----------------------------------------------------------------------------
+
+extern "C" {
+	typedef int (*FuncType)( char * input, char * output, int outsize  );
+}
+
+//----------------------------------------------------------------------------
+// Call command
+//----------------------------------------------------------------------------
+
+class CallCommand : public Command {
 
 	public:
 
@@ -29,6 +41,17 @@ class CallCommand : public Command {
 	private:
 
 		void ProcessFlags( const ALib::CommandLine & cmd );
+		int CallOnFields( CSVRow & row );
+		int CallOnRow( CSVRow & row );
+		enum class ArgType { Field, Row };
+		std::string mDLL, mFuncName;
+		ArgType mArgType;
+		FieldList mFields;
+		bool mReplace;
+		FuncType mFunc;
+
+		static const unsigned int OUTBUF_SIZE = 2048;
+		static char mOutBuf[ OUTBUF_SIZE ];
 
 };
 
