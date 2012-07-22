78a8188dfbc19b48e72d7911e79a1d3004157875
Neil Butterworth <nbutterworth1953@gmail.com>
2012-07-22 15:05:20 +0100

    call command now working, at least experimentally

--- csvfix/inc/csved_call.h
+++ csvfix/inc/csved_call.h
@@ -2,6 +2,7 @@
 // csved_call.h
 //
 // call dll function from csvfix
+// currently windows-only code
 //
 // Copyright (C) 2012 Neil Butterworth
 //---------------------------------------------------------------------------
@@ -17,11 +18,29 @@ namespace CSVED {
 
 
 //----------------------------------------------------------------------------
-// Function in DLL needs to look like this
+// Function in DLL needs to look like this. CSVfix passes the number
+// of fields to the DLL as 'ifc' and the list of fields as 'input'. Each
+// field in 'input' is terminated by a null byte. The DLL must not change or free
+// this data. CSVfix also passes the maximum size of the output buffer
+// as 'outsize' - the DLL must not write beyound this size.
+//
+// The DLL function must return fields in 'output', each field being terminated
+// with a null-byte. The number of fieds must be returned in 'ofc'. Neither
+// 'ofc' or 'output' must be freed by the DLL.
+//
+// The function must return zero on success. A negitive return value means
+// that any returned fields must not be appended to the existing row. A positive
+// return value indicates a fatal error, in which case CSVFix will terminare
+// with an error message.
 //----------------------------------------------------------------------------
 
 extern "C" {
-	typedef int (*FuncType)( int ifc, const char * input, int * ofc, char * output, int outsize  );
+	typedef int (*FuncType)
+		( int ifc, 					// number of fields in input
+		  const char * input, 		// each field must end with \0
+		  int * ofc, 				// DLL must set to number of output fields
+		  char * output, 			// fixed-sized output data
+		  int outsize  );			// fixed size of output data
 }
 
 //----------------------------------------------------------------------------
@@ -42,11 +61,12 @@ class CallCommand : public Command {
 
 		void ProcessFlags( const ALib::CommandLine & cmd );
 		int CallOnFields( CSVRow & row );
+
 		std::string mDLL, mFuncName;
 		FieldList mFields;
 		FuncType mFunc;
 
-		static const unsigned int OUTBUF_SIZE = 2048;
+		static const unsigned int OUTBUF_SIZE = 4096;
 		static char mOutBuf[ OUTBUF_SIZE ];
 
 };
