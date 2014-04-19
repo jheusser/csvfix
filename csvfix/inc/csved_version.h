bb0c442148dec5dd77fca486a34a241439b4b74b
neil butterworth <neilb@cordelia>
2014-04-19 15:26:18 +0100

    pre rel 16 stuff

--- csvfix/inc/csved_version.h
+++ csvfix/inc/csved_version.h
@@ -10,9 +10,9 @@
 #define INC_CSVED_VERSION_H
 
 const char * const CSVED_NAME 	= "CSVfix";
-const char * const CSVED_VERS 	= "1.5";
+const char * const CSVED_VERS 	= "1.6";
 const char * const CSVED_VDATE 	= __DATE__;
-const char * const CSVED_CPYR 	= "Copyright (C) 2012 Neil Butterworth";
+const char * const CSVED_CPYR 	= "Copyright (C) 2014 Neil Butterworth";
 const char * const CSVED_NOWAR 	=
 	"This is free software; see the source for copying conditions. There is NO\n"
 	"warranty, not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.";
